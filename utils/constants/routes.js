3dc31f546168220b0bcb9b614a296c8f05bdb5df
PS Prashant <98733470+PS-PrashantFSD@users.noreply.github.com>
2024-09-29 13:03:55 +0530

    Update routes.js

--- utils/constants/routes.js
+++ utils/constants/routes.js
@@ -1,32 +1,32 @@
 export default [
   {
     id: 'not-humble',
-    title: 'not humble',
-    path: '/projects/not-humble',
+    title: 'Home',
+    path: '/',
     video: 'featured-video.mp4',
   },
   {
     id: 'bleeping-easy',
-    title: 'bleeping easy',
-    path: '/projects/bleeping-easy',
+    title: 'About Us',
+    path: '/About',
     video: 'easy.mp4',
   },
   {
     id: 'make-it-zero',
-    title: 'make it zero',
-    path: '/projects/make-it-zero',
+    title: 'Services',
+    path: '/Services',
     video: 'make-it-zero.mp4',
   },
   {
     id: 'it-takes-an-island',
-    title: 'it takes an island',
-    path: '/projects/it-takes-an-island',
+    title: 'Nexar - ar card',
+    path: '/nexar',
     video: 'it-takes-an-island.mp4',
   },
   {
     id: '50-beaches',
-    title: '50 beaches',
-    path: '/projects/50-beaches',
+    title: 'Contact Us',
+    path: '/conatct',
     video: '50-beaches.mp4',
   },
 ];
